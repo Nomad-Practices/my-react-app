4dd2077bae3e2a19fa6d9c3ead8de8f790a9772a
sjk <jkshin0903@gmail.com>
2022-02-10 22:11:38 +0900

    practice
    simple todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => {
-      console.log("destroyed");
-    };
-  }, []);
-  return <h1>hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((curr) => !curr);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!todo) return;
+    setTodos((curr) => [...curr, todo]);
+    setTodo("");
   };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{!showing ? "SHOW" : "HIDE"}</button>
+      <h1>my to-dos : {todos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button type="submit">Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
